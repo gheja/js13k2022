1fa6d18789b1f2b1f63ea8415be07eb393d958b9
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-03 22:43:25 +0200

    Added GameObjectContainer

---
+++ src/game_object_container.ts
@@ -0,0 +1,10 @@
+class GameObjectContainer extends GameObject
+{
+    domObject2: HTMLElement;
+
+    constructor(position: Vec2D)
+    {
+        super("", position);
+        this.domObject2 = newSpriteDomObject(this.domObject, 0, 18, 10, 10);
+    }
+}
