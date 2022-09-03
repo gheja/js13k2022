6f12715f43165166ebf41c0bf7fcf96667fa63af
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-03 02:34:58 +0200

    Added embed_gfx.sh

---
+++ embed_gfx.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+tmp=`readlink -f "$0"`
+dir=`dirname $tmp`
+source_dir="${dir}/src"
+
+{
+echo -n "const GFX_SPRITES = \"data:image/png;base64,"
+cat "${source_dir}/graphics/sprites_optimized.png" | base64 --wrap 0
+echo "\";"
+} > "${source_dir}/consts_graphics.ts"
