f8e77c227c65942cb0ce4617e1fcbbb419012f4f
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-05 00:00:21 +0200

    GameObjectMeat: added, hello

---
+++ src/game_object_meat.ts
@@ -0,0 +1,10 @@
+class GameObjectMeat extends GameObject
+{
+    domObject2: HTMLElement;
+
+    constructor(position: Vec2D)
+    {
+        super(position, 10, 18, "Meat", "Some tasty meat, kinda raw though.");
+        this.domObject2 = newSpriteDomObject(this.domObject, 30, 0, 10, 18);
+    }
+}
