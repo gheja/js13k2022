e7e84ba4f96181b901e952f1c3787d5580d63707
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-05 02:38:39 +0200

    Added cooking status animations

--- src/game_object_container.ts
+++ src/game_object_container.ts
@@ -45,6 +45,32 @@ class GameObjectContainer extends GameObject
         if (this.isOnFire)
         {
             this.cookedForTicks++;
+
+            // TODO: optimize this... although zip should be pretty efficient here
+            if (this.cookedForTicks == Math.floor(this.cookedForTarget * 0.1))
+            {
+                emitParticle(this.position.x, this.position.y, 60, 18, 10, 10, "a3", 3000);
+            }
+            else if (this.cookedForTicks == Math.floor(this.cookedForTarget * 0.7))
+            {
+                emitParticle(this.position.x, this.position.y, 70, 18, 10, 10, "a3", 3000);
+            }
+            else if (this.cookedForTicks == Math.floor(this.cookedForTarget * 0.9))
+            {
+                emitParticle(this.position.x, this.position.y, 80, 18, 10, 10, "a3", 3000);
+            }
+            else if (this.cookedForTicks == this.cookedForTarget)
+            {
+                emitParticle(this.position.x, this.position.y, 90, 18, 10, 10, "a3", 3000);
+            }
+            else if (this.cookedForTicks == Math.floor(this.cookedForTarget * 1.1))
+            {
+                emitParticle(this.position.x, this.position.y, 100, 18, 10, 10, "a3", 3000);
+            }
+            else if (this.cookedForTicks == Math.floor(this.cookedForTarget * 1.2))
+            {
+                emitParticle(this.position.x, this.position.y, 110, 18, 10, 10, "a3", 3000);
+            }
         }
         
         if (_game.ticks % 8 == 0)
