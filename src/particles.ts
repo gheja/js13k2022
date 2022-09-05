f78baebb20241f7b2f5096454c06d4defde43e7a
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-05 02:26:02 +0200

    Updated emitParticle()

--- src/particles.ts
+++ src/particles.ts
@@ -3,13 +3,13 @@ function destroyThis()
 	this.parentNode.removeChild(this);
 }
 
-function emitParticle(x: number, y: number, className: string, duration: number)
+function emitParticle(x: number, y: number, spriteX: number, spriteY: number, width: number, height: number, animation: string, duration: number)
 {
 	let a: HTMLElement;
 	
-	a = newSpriteDomObject(_divLayer, 5, 14, 3, 3)
-	a.style.transform = "translateX(" + _z(x) + "px) translateY(" + _z(_floorHeight / 2 - 3) + "px) translateZ(" + _z(y) + "px) ";
-	a.style.animation = "dust2 0.4s";
+	a = newSpriteDomObject(_divLayer, spriteX, spriteY, width, height);
+	a.style.transform = "translateX(" + _z(x) + "px) translateY(" + _z(_floorHeight / 2 - height) + "px) translateZ(" + _z(y) + "px) ";
+	a.style.animation = animation + " " + duration + "ms forwards";
 
 	window.setTimeout(destroyThis.bind(a), duration);
 }
