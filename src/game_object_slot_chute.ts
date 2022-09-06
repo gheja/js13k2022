6b281d8ca487302caf48e02d77656260bd69d3ff
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-07 01:57:51 +0200

    Added a dummy response

--- src/game_object_slot_chute.ts
+++ src/game_object_slot_chute.ts
@@ -14,6 +14,9 @@ class GameObjectSlotChute extends GameObjectSlot
     catch(obj: GameObject)
     {
         // TODO: evaulate points
+
+        dialogStart([[ 1, 1, "Oh nice!" ], [ 1, 1, "It is a bit raw, though... 4/5 stars. (But this is just a dummy text.)" ]]);
+
         if (obj instanceof GameObjectContainer && (obj as GameObjectContainer).recipe)
         {
             (obj as GameObjectContainer).recipe.status = RECIPE_STATUS_DONE;
