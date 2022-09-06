2196385f05eaee6b4a5b51c377a3d0908f489328
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-07 01:45:23 +0200

    Reworked pan and pot handling

---
+++ src/game_object_container_pan.ts
@@ -0,0 +1,7 @@
+class GameObjectContainerPan extends GameObjectContainer
+{
+    constructor(position: Vec2D)
+    {
+        super(position, 10, 10, "Pan", "A frying pan to cook some delicious food.", 170, 18);
+    }
+}
---
+++ src/game_object_container_pot.ts
@@ -0,0 +1,7 @@
+class GameObjectContainerPot extends GameObjectContainer
+{
+    constructor(position: Vec2D)
+    {
+        super(position, 10, 10, "Small pot", "A container to fill with ingredients.", 0, 18);
+    }
+}
