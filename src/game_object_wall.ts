046ba9b1f94ff53b92a65cc0684b0a7e2e7196c4
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-09 23:07:12 +0200

    GameObjectWall: now supports multiple block version

--- src/game_object_wall.ts
+++ src/game_object_wall.ts
@@ -5,7 +5,7 @@ class GameObjectWall extends GameObject
     domObjectTop: HTMLElement;
     domObjectRight: HTMLElement;
 
-    constructor(position: Vec2D)
+    constructor(position: Vec2D, width: number=1, height: number=1, zHeight: number=1)
     {
         super(position, 10, 10, "e", "e");
 
@@ -22,5 +22,23 @@ class GameObjectWall extends GameObject
 
         this.domObjectFront = newSpriteDomObject(this.domObject, 30, 70, 10, 10);
         this.domObjectFront.className = "bf";
+
+        // x
+        this.domObjectFront.style.width = _z(width * 10) + "px";
+        this.domObjectTop.style.width = _z(width * 10) + "px";
+        this.domObjectRight.style.left = _z((width - 1) * 10) + "px";
+
+        // y
+        // TODO...
+
+        // z
+        this.domObjectTop.style.top = - _z((zHeight - 1) * 10) + "px";
+        this.domObjectFront.style.top = this.domObjectTop.style.top;
+        this.domObjectLeft.style.top = this.domObjectFront.style.top;
+        this.domObjectRight.style.top = this.domObjectFront.style.top;
+
+        this.domObjectFront.style.height = _z(zHeight * 10) + "px";
+        this.domObjectRight.style.height = this.domObjectFront.style.height;
+        this.domObjectLeft.style.height = this.domObjectFront.style.height;
     }
 }
