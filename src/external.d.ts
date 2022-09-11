abc75132278d3d3764104e22cebd7a069514dd94
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-11 23:11:59 +0200

    Updated the external.d.ts definitions

--- src/external.d.ts
+++ src/external.d.ts
@@ -1,23 +1,23 @@
 // These classes, functions and variables are not part of the code but are
-// included in external libraries or imports. This makes sure VS Code knows
-// about them and they can be used in the project.
+// included in external libraries, imports or are added to the final bundle
+// by the build.sh build process.
+//
+// This makes sure VS Code knows about them and they can be used in the
+// project.
 //
 // https://stackoverflow.com/a/13087849 - thanks!
 
 interface dummyFunction { (): any; }
+interface zzfxFunction { (...parameters): any; }
 
 declare var coilInit: dummyFunction;
 
-// declare var nearInit: dummyFunction;
-// declare var nearLogin: dummyFunction;
-// declare var nearTip: dummyFunction;
-// declare var _nearNetName: string;
+declare var na: dummyFunction; // nearActive
+declare var ni: dummyFunction; // nearInit
+declare var nl: dummyFunction; // nearLogin
+declare var nt: dummyFunction; // nearTip
+declare var nn: string; // _nearNetName
 
-// minimized versions
-declare var na: dummyFunction;
-declare var ni: dummyFunction;
-declare var nl: dummyFunction;
-declare var nt: dummyFunction;
-declare var nn: string;
+declare var io: dummyFunction; // socket.io
 
-declare var io: dummyFunction;
+declare var zzfx: zzfxFunction;
