c065d00225955673f495951c736c794c6ceb05e6
Gabor Heja <gheja@d10wsl.git.stdio.hu>
2022-09-13 03:28:22 +0200

    Trying to make Google Closure Compiler happier - saves 8 bytes of final ZIP file

--- src/game_object_wall.ts
+++ src/game_object_wall.ts
@@ -14,36 +14,29 @@ class GameObjectWall extends GameObject
         this.collidable = true;
 
         this.domObjectTop = newSpriteDomObject(this.domObject, 40, 70, 10, 10);
-        this.domObjectTop.className = "bt";
-
         this.domObjectLeft = newSpriteDomObject(this.domObject, 30, 70, 10, 10);
-        this.domObjectLeft.className = "bl";
-
         this.domObjectRight = newSpriteDomObject(this.domObject, 30, 70, 10, 10);
-        this.domObjectRight.className = "br";
-
         this.domObjectFront = newSpriteDomObject(this.domObject, 30, 70, 10, 10);
+
+        this.domObjectTop.className = "bt";
+        this.domObjectLeft.className = "bl";
+        this.domObjectRight.className = "br";
         this.domObjectFront.className = "bf";
 
         this.repeatWidth = width;
         // this.repeatHeight = height;
+
+        // less readable but Google Closure Compiler likes more
         
         // x
-        this.domObjectFront.style.width = _z(width * 10) + "px";
-        this.domObjectTop.style.width = _z(width * 10) + "px";
+        this.domObjectFront.style.width = this.domObjectTop.style.width = _z(width * 10) + "px";
         this.domObjectRight.style.left = _z((width - 1) * 10) + "px";
 
         // y
         // TODO...
 
         // z
-        this.domObjectTop.style.top = - _z((zHeight - 1) * 10) + "px";
-        this.domObjectFront.style.top = this.domObjectTop.style.top;
-        this.domObjectLeft.style.top = this.domObjectFront.style.top;
-        this.domObjectRight.style.top = this.domObjectFront.style.top;
-
-        this.domObjectFront.style.height = _z(zHeight * 10) + "px";
-        this.domObjectRight.style.height = this.domObjectFront.style.height;
-        this.domObjectLeft.style.height = this.domObjectFront.style.height;
+        this.domObjectTop.style.top = this.domObjectFront.style.top = this.domObjectLeft.style.top = this.domObjectRight.style.top = - _z((zHeight - 1) * 10) + "px";
+        this.domObjectFront.style.height = this.domObjectRight.style.height = this.domObjectLeft.style.height = _z(zHeight * 10) + "px";
     }
 }
